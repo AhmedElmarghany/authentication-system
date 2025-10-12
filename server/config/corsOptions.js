87f903ee798f7651f8e51fb2ad07c5e29eb4c76f
Ahmed Elmarghany <amelmarghany@gmail.com>
2025-10-12 22:02:17 +0300

    create APIs

---
+++ server/config/corsOptions.js
@@ -0,0 +1,18 @@
+const allowedOrigins = require("./allowedOrigins");
+const corsOptions = {
+    origin: (origin, callback)=>{
+        // check if there is more than one domain
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            callback(null, true);
+        }else{
+            // throw an error if an attemp is made to access the server by unregistered domain name
+            callback(new Error("Not allowed ny CORS"));
+        }
+    },
+    // accept any credentials come with headers or cookies
+    Credential: true,
+    optionsSuccessStatus: 200
+}
+
+
+module.exports = corsOptions;
