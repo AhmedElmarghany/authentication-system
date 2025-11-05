be253b047f782c7ac905c82bf0c2e0bdd6ce331b
Ahmed Elmarghany <amelmarghany@gmail.com>
2025-11-06 01:35:39 +0200

    Edit configs for deployment

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://authcore-app.netlify.app", "http://localhost:4173", "http://localhost:5173"];
 
 module.exports = allowedOrigins;
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000
+//   }
+// })
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    port: 3000
-  }
 })
